1b5c289e09009037316b8a95f84c450d60566a8d
Robert Johnston <robertjohnston320@hotmail.com>
2016-05-04 21:02:12 +1200

    Start work on ability to search and display results. No real implementation yet, just a new page with a search bar and a slider for price range. Also, index.js has been modified so you can actually get to the new search page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET search page. */
+router.get('/searchpage', function(req, res, next) {
+  res.render('searchpage', { title: 'Express' });
+});
+
 module.exports = router;
+
+router.get('/search', function(req, res, next) {
+    // get the search query
+   //make it suitable for the DB
+  //query the DB
+  // return results
+  //create each listing for the page based  of each result
+  //
+  var exampleResult1 = {image:"1.jpg", price: 200, name:"Bondage Hat"};
+  var exampleResult2 = {image:"2.jpg", price: 100, name:"S&M Hat"};
+  var resultsArray = [exampleResult1, exampleResult2 ];
+  res.render('searchpage', { results : resultsArray});
+
+  });
